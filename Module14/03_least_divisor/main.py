9c5c1b9015a9599f032ab8922241f9ef4c813a0e
Admin <https://gitlab.skillbox.ru/sher_ttt/Python_Basic>
2025-02-24 12:17:09 +0500

    Домашняя работа по модулю

--- Module14/03_least_divisor/main.py
+++ Module14/03_least_divisor/main.py
@@ -1,1 +1,12 @@
 # TODO здесь писать код
+def least_divisor(number):
+    for i in range(2, number + 1):
+        if number % i == 0:
+            return i
+
+
+
+number = int(input('Введите число: '))
+
+print('Наименьший делитель, отличный от единицы:', least_divisor(number))
+
