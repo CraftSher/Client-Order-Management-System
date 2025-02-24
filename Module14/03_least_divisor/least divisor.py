3ed94d609701620cff2d84cbb9acb3480e6b2622
Admin <https://gitlab.skillbox.ru/sher_ttt/Python_Basic>
2025-02-24 12:39:48 +0500

    Add code for least divisor

---
+++ Module14/03_least_divisor/least divisor.py
@@ -0,0 +1,10 @@
+def least_divisor(number):
+    for i in range(2, number + 1):
+        if number % i == 0:
+            return i
+
+
+
+number = int(input('Введите число: '))
+
+print('Наименьший делитель, отличный от единицы:', least_divisor(number))
