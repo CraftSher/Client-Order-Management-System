fa24490ae2861aa9754d56760b1ba7a973b68529
Admin <https://gitlab.skillbox.ru/sher_ttt/Python_Basic>
2025-04-22 15:44:09 +0500

    Added OS information

--- Module23/05_reliable_calculations/main.py
+++ Module23/05_reliable_calculations/main.py
@@ -1,10 +1,26 @@
-# TODO здесь писать код
+import math
 
-# Здесь создайте функцию get_sage_sqrt
+def calculate_square_root(number):
+    if not isinstance(number, (int, float)):
+        raise TypeError("Введенное значение не является числом")
+    if number < 0:
+        raise ValueError("Невозможно вычислить квадратный корень отрицательного числа")
+    else:
+        return math.sqrt(number)
 
+def process_numbers(numbers):
+    results = []
+    for number in numbers:
+        try:
+            sqrt_result = calculate_square_root(number)
+            results.append(f"Квадратный корень из {number}: {sqrt_result}")
+        except (ValueError, TypeError) as e:
+            results.append(f"Ошибка для {number}: {e}")
+        except Exception as exc:
+            results.append(f"Непредвиденная ошибка для {number}: {exc}")
+    return results
 
-# Тестовые случаи
 numbers = [16, 25, -9, 0, 4.5, "abc"]
-for number in numbers:
-    result = get_sage_sqrt(number)
-    print(f"Квадратный корень numbers {number}: {result}")
+results = process_numbers(numbers)
+for result in results:
+    print(result)
